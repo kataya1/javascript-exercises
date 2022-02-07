8501792dd12895b913239cf0b6ef6942d8e4893a
kataya1 <mohamedkataya1@gmail.com>
2022-02-07 13:07:33 +0200

    little exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0 
+    let oldestperson 
+    let age
+    let now = new Date()
 
-};
+    let nowYear = now.getFullYear()
+    for ( p of people) {
+       age = "yearOfDeath" in p ? p.yearOfDeath - p.yearOfBirth : nowYear - p.yearOfBirth
+       if (age > oldestAge ) {
+           oldestAge = age
+             oldestperson = p
+        }
+    }
+    return oldestperson
+};  
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,20 @@
-const caesar = function() {
-
+const dispalceLetter = (letter, d) => String.fromCharCode(((letter.charCodeAt(0) - 'a'.charCodeAt(0) + d) % 26) + 'a'.charCodeAt(0))
+const caesar = function(string, d) {
+    if ( d < 0) d = 26 + ( d % 26 )
+    let arr = []
+    for( let i = 0; i < string.length; i++){
+        let l = string[i]
+        if( l.charCodeAt(0) >= 97 && l.charCodeAt(0) <= 122)
+            arr.push(dispalceLetter(l, d))
+        
+        else if(l.charCodeAt(0) >= 65 && l.charCodeAt(0) <= 90)
+            arr.push(dispalceLetter(l.toLowerCase(), d).toUpperCase())
+        else
+            arr.push(l)
+    }
+    return arr.join('')
 };
+console.log(caesar('A', 1))  //B
 
 // Do not edit below this line
 module.exports = caesar;
