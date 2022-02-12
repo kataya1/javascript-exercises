b8ac78964bfeb363e95b513963fbe75b75fe4bcc
kataya1 <mohamedkataya1@gmail.com>
2022-02-12 11:18:44 -0500

    array reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,25 @@
-const findTheOldest = function(people) {
-    let oldestAge = 0 
-    let oldestperson 
-    let age
-    let now = new Date()
+// const findTheOldest = function(people) {
+//     let oldestAge = 0 
+//     let oldestperson 
+//     let age
+//     let now = new Date()
 
-    let nowYear = now.getFullYear()
-    for ( p of people) {
-       age = "yearOfDeath" in p ? p.yearOfDeath - p.yearOfBirth : nowYear - p.yearOfBirth
-       if (age > oldestAge ) {
-           oldestAge = age
-             oldestperson = p
-        }
-    }
-    return oldestperson
-};  
-
+//     let nowYear = now.getFullYear()
+//     for ( p of people) {
+//        age = "yearOfDeath" in p ? p.yearOfDeath - p.yearOfBirth : nowYear - p.yearOfBirth
+//        if (age > oldestAge ) {
+//            oldestAge = age
+//              oldestperson = p
+//         }
+//     }
+//     return oldestperson
+// };  
+const findTheOldest = function (people){
+    return people.reduce( (oldest, person) => {
+        return (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth >
+         (oldest.yearOfDeath ?? new Date().getFullYear()) - oldest.yearOfBirth?
+            person: oldest
+    } , {yearOfBirth: 0, yearOfDeath: 0})
+}
 // Do not edit below this line
 module.exports = findTheOldest;
